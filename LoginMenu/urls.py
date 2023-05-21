3c6a98f6d68590d9a7df7d68c79194dd47079501
Isaiah-Samuel <isaiahsamuel342@gmail.com>
2023-05-21 20:09:31 +0100

    push to github

--- LoginMenu/urls.py
+++ LoginMenu/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 
 urlpatterns = [ 
-    path('',views.logIn, name='logIn')
+    path('',views.logIn, name='logIn'),
+    path('register', views.register, name='register')
 ]
--- LoginMenu/views.py
+++ LoginMenu/views.py
@@ -9,3 +9,5 @@ def logIn(request):
     return render(request, 'logIn.html')
 
 
+def register(request):
+    return render(request,'register.html')
